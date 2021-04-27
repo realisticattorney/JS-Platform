7b66c530f7676f1fdae8cd855ce0876e98a45b57
realisticattorney <g.axl.aquila@gmail.com>
2021-04-27 11:01:14 -0300

    Try another loader

--- webpack.config.js
+++ webpack.config.js
@@ -10,14 +10,32 @@ module.exports = {
     publicPath: '/build/',
     filename: 'project.bundle.js',
   },
-
   module: {
-    rules: [
-      {
-        test: [/\.vert$/, /\.frag$/],
-        use: 'raw-loader',
-      },
-    ],
+    rules: [{
+      test: /\.(png|jpg|gif)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          outputPath: 'assets'
+        }
+      }
+    },
+    {
+      test: /\.(mp3|wav|wma|ogg|aif)$/,
+      loader: 'file-loader',
+    },
+    {
+      test: /\.html$/i,
+      loader: 'html-loader',
+    },
+    {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    }, { test: /\.jsx?$/, exclude: /'node_modules'/, loader: 'babel-loader' },
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist')
   },
 
   plugins: [
